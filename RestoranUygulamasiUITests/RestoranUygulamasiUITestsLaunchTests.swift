a33bf7e22473e3a8e9c4f255ce415f877f4d4bdf
hmzaont <hamza_jobs@outlook.com>
2024-05-17 09:44:41 +0300

    Hata düzeltmeleri yapıldı.
    Bazı dosyalarda bulunan hatalı kodlar düzeltilmeye çalışıldı.

--- RestoranUygulamasiUITests/RestoranUygulamasiUITestsLaunchTests.swift
+++ RestoranUygulamasiUITests/RestoranUygulamasiUITestsLaunchTests.swift
@@ -1,10 +1,3 @@
-//
-//  OnlineGroceriesSwiftUIUITestsLaunchTests.swift
-//  OnlineGroceriesSwiftUIUITests
-//
-//  Created by CodeForAny on 30/07/23.
-//
-
 import XCTest
 
 final class OnlineGroceriesSwiftUIUITestsLaunchTests: XCTestCase {
@@ -20,10 +13,6 @@ final class OnlineGroceriesSwiftUIUITestsLaunchTests: XCTestCase {
     func testLaunch() throws {
         let app = XCUIApplication()
         app.launch()
-
-        // Insert steps here to perform after app launch but before taking a screenshot,
-        // such as logging into a test account or navigating somewhere in the app
-
         let attachment = XCTAttachment(screenshot: app.screenshot())
         attachment.name = "Launch Screen"
         attachment.lifetime = .keepAlways
